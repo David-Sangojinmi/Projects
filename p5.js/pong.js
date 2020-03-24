67239ccdc14a431c3ba0ca15212deb66a8292423
David S <dsangojinmi26@gmail.com>
2020-03-24 16:37:24 +0000

    Update pong.js

---
+++ p5.js/pong.js
@@ -0,0 +1,43 @@
+let paddleA = {
+    x: 50,
+    y: 200,
+    speed: 5,
+}
+
+let paddleB = {
+    x: 50,
+    y: 450,
+    speed: 5,
+}
+
+function setup() {
+    createCanvas(500, 400);
+    rectMode(CENTER);
+}
+
+function draw() {
+    background(0);
+
+    //Call the paddles
+    rect(paddleA.x, paddleA.y, 20, 50);
+    move();
+}
+
+//function padA() {
+//let x = paddleA.x;
+//let y = paddleA.y;
+
+//rect(paddleA.x, paddleA.y, 20, 50);
+
+function move() {
+    if (keyIsPressed === true) {
+        if (keyCode === UP_ARROW) {
+            paddleA.y -= paddleA.speed;
+        }
+    }
+}
+//}
+
+function padB() {
+
+}
