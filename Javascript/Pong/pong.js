71a7f868419024ff6afee34734fcbd588af54e81
David S <dsangojinmi26@gmail.com>
2020-03-24 21:34:41 +0000

    Update pong.js

--- Javascript/Pong/pong.js
+++ Javascript/Pong/pong.js
@@ -8,13 +8,14 @@ let ball = {
     x: 450,
     y: 200,
     diameter: 20,
-    speed: 7
+    xspeed: 2,
+    yspeed: 3
 };
 
 function setup() {
-    createCanvas(900, 400);
+    createCanvas(900, 390);
     rectMode(CENTER);
-    ellipsdeMode(CENTER);
+    ellipseMode(CENTER);
 }
 
 function draw() {
@@ -30,6 +31,7 @@ function draw() {
 }
 
 function paddleMove() {
+    // Paddle movement
     if (keyIsPressed === true) {
         if (keyCode === UP_ARROW) {
             paddleA.y -= paddleA.speed;
@@ -41,6 +43,21 @@ function paddleMove() {
 }
 
 function ballMove() {
-
-
+    // Horizontal movement
+    ball.x -= ball.xspeed;
+    if ((ball.x-10) <= (paddleA.x+9)) {
+        ball.xspeed = ball.xspeed * -1;
+    }
+    if ((ball.x+10) >= 890) {
+        ball.xspeed = ball.xspeed * -1;
+    }
+    // Vertical movement
+    ball.y -= ball.yspeed;
+    if (ball.y - 10 <= 10) {
+        ball.yspeed = ball.yspeed * -1;
+    }
+    if (ball.y + 10 >= 380) {
+        ball.yspeed = ball.yspeed * -1;
+    }
+    // Collision detection
 }
