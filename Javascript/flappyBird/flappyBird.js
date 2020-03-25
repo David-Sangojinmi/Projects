6116fad644d1770e19093c0483f11e31d090f45f
David S <dsangojinmi26@gmail.com>
2020-03-25 12:27:47 +0000

    Started flappy bird project

--- Javascript/flappyBird/flappyBird.js
+++ Javascript/flappyBird/flappyBird.js
@@ -1,38 +1,38 @@
-var ball = {
-    x: 450,
-    y: 200,
-    diameter: 20,
-    opacity: 255,
-    numOf: 10,
-    xspeed: 4,
-    yspeed: 6
+var col1 = {
+    x1: 850,
+    y1: 0,
+    x2: 900,
+    y2: 100,
+    speed: 3
+};
+
+var col2 = {
+    x1: 850,
+    y1: 180,
+    x2: 900,
+    y2: 390,
+    speed: 3
 };
 
 function setup() {
     createCanvas(900, 390);
+    rectMode(CORNERS);
     ellipseMode(CENTER);
-    background(0);
+    //background(39, 201, 204);
 }
 
 function draw() {
-    ball.opacity = random(0, 255);
-    fill(255, ball.opacity);
+    background(39, 201, 204);
 
-    noStroke();
-    ball.x = random(0, width);
-    ball.y = random(0, height);
-    ball.diameter = random(5, 30);
-    spawnBalls();
+    // Column height generation
+    col1.y2 = random(25, 285);
+    col2.y1 = 390 - col1.y2 - 80;
     
-}
-
-function spawnBalls() {
-    ellipse(ball.x, ball.y, ball.diameter);
-}
-
-function keyPressed() {
-    if (key === 'p') {
-        clear();
-        background(0);
-    }
+    // Column generation
+    rect(col1.x1, col1.y1, col1.x1+50, col1.y2);
+    rect(col2.x1, col2.y1, col2.x1+50, col2.y2);
+    
+    // Columns moving along the screen
+    col1.x1 -= col1.speed;
+    col2.x1 -= col2.speed;
 }
