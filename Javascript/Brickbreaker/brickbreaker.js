bb17a116f4db9948161a07f4b2e9d438f7507c4f
David S <dsangojinmi26@gmail.com>
2020-04-05 21:24:32 +0100

    Update brickbreaker.js

--- Javascript/Brickbreaker/brickbreaker.js
+++ Javascript/Brickbreaker/brickbreaker.js
@@ -2,10 +2,29 @@ let gameBegin = true;
 let gameActive = false;
 let gameFinish = false;
 
+let paddle = {
+    xpos: 300,
+    ypos: 650,
+    speed: 5,
+};
+
+let bricks = [];
+
+let game = {
+    level: 1,
+    score: 0,
+    lives: 3,
+};
+
 function setup() {
     createCanvas(600, 700);
     rectMode(CENTER);
     ellipseMode(CENTER);
+
+    for (let i = 0; i < 2; i++) {
+        let x = 200 + 200 * i;
+        bricks[i] = new Bricks(Bricks.x, 300, Bricks.w, Bricks.h);
+    }
 }
 
 function draw() {
@@ -15,10 +34,41 @@ function draw() {
     gameLoop();
 }
 
+class Bricks {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = 40;
+        this.h = 60;
+    }
+
+    show() {
+        stroke(145, 0, 0);
+        strokeWeight(2);
+        fill(189, 9, 9);
+        rect();
+    }
+
+    destroy() {
+        noFill();
+    }
+}
+
 function gameStart() {
     // Screen for the start of the game
+    textAlign(CENTER);
+    textStyle(BOLD);
+    textSize(40);
+    fill(200, 200, 200);
+    textFont("monospace");
+    text("BRICK BREAKER", 300, 200);
     rect(67, 67, 67, 67);
 
+    // Instructions
+    /* Welcome! Use the left and right arrow keys to move the paddle left and right. You start at level 1 and you have three lives. To get points, use your paddle to hit the ball and break the bricks. You have three lives before its game over. Press ENTER to start. Good luck!
+     */
+
+    // Moving to gameplay
     if (keyIsPressed === true) {
         if (keyCode === ENTER) {
             gameBegin = false;
@@ -29,21 +79,28 @@ function gameStart() {
 
 function gamePlay() {
     // Screen for when the game is active
-    // Maybe include different stages
-    rect(456, 456, 67, 67);
 
+    // Show the level, points and lives
+
+    // Draw the paddle and bricks
+    rect(paddle.xpos, paddle.ypos, 80, 20, 5, 5, 10, 10);
+    for (let i = 0; i < 2; i++) {
+        bricks[i];
+    }
+
+    // Paddle moving
     if (keyIsPressed === true) {
-        if (keyCode === UP_ARROW) {
-            gameBegin = true;
-            gameActive = false;
+        if (keyCode === LEFT_ARROW) {
+            paddle.xpos -= paddle.speed;
+        }
+        if (keyCode === RIGHT_ARROW) {
+            paddle.xpos += paddle.speed;
         }
     }
 }
 
 function gameEnd() {
     // Screen for when the game has finished
-
-
 }
 
 function gameLoop() {
