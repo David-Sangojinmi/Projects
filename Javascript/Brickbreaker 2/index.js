b97d9786aeffccb6b0a7b87f91b25bbe91fd5270
David S <dsangojinmi26@gmail.com>
2020-04-09 15:47:53 +0100

    Update index.js

--- Javascript/Brickbreaker 2/index.js
+++ Javascript/Brickbreaker 2/index.js
@@ -8,4 +8,19 @@ const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
-paddle.draw();
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, 800, 600);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
