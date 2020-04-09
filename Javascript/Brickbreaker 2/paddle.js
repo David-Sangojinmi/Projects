2d24f710a2e86ef5a3599ff6eed842d7c82d1298
David S <dsangojinmi26@gmail.com>
2020-04-09 15:47:50 +0100

    Update paddle.js

--- Javascript/Brickbreaker 2/paddle.js
+++ Javascript/Brickbreaker 2/paddle.js
@@ -4,12 +4,17 @@ export default class Paddle {
         this.height = 30;
 
         this.position = {
-            x: gameWidth/2 - this.width/2;
-            y: gameHeight - this.height - 10;
-        }
+            x: gameWidth / 2 - this.width / 2,
+            y: gameHeight - this.height - 10,
+        };
     }
 
     draw(ctx) {
+        ctx.fillStyle = "#f00";
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update(deltaTime) {
+        if (!deltaTime) return;
     }
 }
