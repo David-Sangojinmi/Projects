35cff635e8f1911648a38634fa7f457c2549d339
David S <dsangojinmi26@gmail.com>
2020-04-14 13:29:26 +0100

    Init problem 7

---
+++ Euler/problem7.py
@@ -0,0 +1,28 @@
+# tst = input('Number ')
+# gdprm = []
+
+# # Initialize a list
+# primes = []
+# for possiblePrime in range(2, 21):
+    
+#     # Assume number is prime until shown it is not. 
+#     isPrime = True
+#     for num in range(2, possiblePrime):
+#         if possiblePrime % num == 0:
+#             isPrime = False
+#             break
+      
+#     if isPrime:
+#         primes.append(possiblePrime)
+
+# # Test prime number for divisibility
+# abcd = -1
+
+# # Creating a loop to test run all primes for divisibility
+# while (tst % gdprm[abcd] == 0) == False:
+#   abcd -= 1
+  
+#   if (tst % gdprm[abcd] == 0) == True:
+#     gdnum.append(gdprm[abcd])
+    
+# print(gdnum)
