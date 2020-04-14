7aedb97e29e8291a8c3018597349ed7c5d1132e1
David S <dsangojinmi26@gmail.com>
2020-04-14 13:21:50 +0100

    Solved problem 3

--- Euler/problem3.py
+++ Euler/problem3.py
@@ -1,19 +1,20 @@
-tst = int(input('Number: '))
+tst = int(input('Number to test: '))
 gdnum = []
 
 # Initialize a list
 gdprm = []
-for possiblePrime in range(2, int((tst/2))):
+for possiblePrime in range(2, int(tst**0.5)):
     
     # Assume number is prime until shown it is not. 
     isPrime = True
-    for num in range(2, int(possiblePrime ** 0.5) + 1):
+    for num in range(2, int(possiblePrime**0.5) + 1):
         if possiblePrime % num == 0:
             isPrime = False
             break
       
     if isPrime:
         gdprm.append(possiblePrime)
+        print(possiblePrime)
 
 # Test prime number for divisibility
 abcd = -1
@@ -25,6 +26,4 @@ while (tst % gdprm[abcd] == 0) == False:
   if (tst % gdprm[abcd] == 0) == True:
     gdnum.append(gdprm[abcd])
     
-print(gdnum)
-
-
+print("Largest prime factor is:", *gdnum)
