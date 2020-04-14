d13d16189c522e184b64200b24bb1f4ff17c80e1
David S <dsangojinmi26@gmail.com>
2020-04-14 13:25:56 +0100

    Init problem 5

---
+++ Euler/problem5.py
@@ -0,0 +1,13 @@
+# what: What is the smallest positive number that is evenly divisible
+# by all the numbers from 1 to 20?
+
+# foundAns = False
+# testNum = 1
+
+# def evenDivisible(divisble):
+#     if (divisible % 2 == 0):
+#         foundAns = True
+#     else:
+#         pass
+
+# def findDivs(testNum):
