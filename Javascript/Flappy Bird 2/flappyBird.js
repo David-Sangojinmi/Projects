09f98e2e4fd44dd3b55167f36f4e300f73ebe4ea
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-17 00:56:40 +0100

    Update flappyBird.js

--- Javascript/Flappy Bird 2/flappyBird.js
+++ Javascript/Flappy Bird 2/flappyBird.js
@@ -1,3 +1,9 @@
+/*
+Video: https://www.youtube.com/watch?v=L07i4g-zhDA&list=PLt4757glfbhHkfz7dqojMbnBdgUnFih4B&index=5&t=26s
+Time of video: 23:26
+Code: https://github.com/CodeExplainedRepo/FlappyBird-JavaScript/blob/master/flappyBird.js
+*/
+
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
@@ -14,38 +20,70 @@ fg.src = "images/fg.png";
 pipeNorth.src = "images/pipeNorth.png";
 pipeSouth.src = "images/pipeSouth.png";
 
-// Draw images
-ctx.drawImage(imageName, 100, 150, 50, 50);
+// Load audio
+var fly = new Audio();
+var scor = new Audio();
+
+fly.src = "sounds/fly.mp3";
+scor.src = "sounds/score.mp3";
+
+// Important variables
+var gap = 85;
+var constant  = pipeNorth.height + gap;
+
+var bX = 10;
+var bY = 150;
+var gravity = 1;
+
+// When key is down
+document.addEventListener("keydown", moveUp);
+
+function moveUp() {
+    bY -= 20;
+}
 
 // Generating pipes
 var pipe = [];
+
 pipe[0] = {
-    x: cvs.clientWidth,
+    x: cvs.width,
     y: 0
 }
 
 function draw() {
+    ctx.drawImage(bg, 0, 0); // Draw background
+
     for (var i = 0; i < pipe.length; i++) {
+        constant = pipeNorth.height + gap;
         ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
-        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + const);
+        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+        
         pipe[i].x--;
-        if (pipe[i].x == cvs.width - 188) {
-            pipe.push(
+
+        if (pipe[i].x == 125) {
+            pipe.push({
                 x: cvs.width,
-                y: Math.floor(Math.random() * pN.height) - pN.height
-            );
+                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height,
+            });
+        }
+
+        // Detect if there is a collision
+        if () {
+            location.reload();
+        }
+
+        if () {
+            score++;
+            scor.play();
         }
     }
-    ctx.drawImage(bg, 0, 0);  // Draw background
 
-    ctx.drawImage(pipeNorth, pX, pY);  // Top pipe
-    ctx.drawImage(pipeSouth, pX, pY + ###)  // Bottom pipe
-    ctx.drawImage(fg, 0, cvs.height - fg.height);  // Foreground
-    ctx.drawImage(bird, bX, bY);  // Draw bird
+    ctx.drawImage(fg, 0, cvs.height - fg.height); // Foreground
+    ctx.drawImage(bird, bX, bY); // Draw bird
 
     bY += gravity;
-    requestAnimationFrame(draw);
 
+    requestAnimationFrame(draw);
 }
 
 draw();
