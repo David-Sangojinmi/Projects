29470268167b2511ffee9706bc52293470bae99e
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-18 16:25:50 +0100

    Update flappyBird.js

--- Javascript/Flappy Bird 2/flappyBird.js
+++ Javascript/Flappy Bird 2/flappyBird.js
@@ -1,7 +1,8 @@
 /*
 Code: https://github.com/CodeExplainedRepo/FlappyBird-JavaScript/blob/master/flappyBird.js
-TO-DO: Make it so when you click the play button or press a key it moves
-to game play and doesn;t just get stuck on the start screen
+TO-DO: Make it so when you click the play button it moves to game play and
+doesn't just get stuck on the start screen.
+Fix the transition from game play to end screen.
 */
 
 var cvs = document.getElementById("canvas");
@@ -55,129 +56,139 @@ pipe[0] = {
 
 function gameStart() {
     // Game title and instructions
-    function drawStart() {
-        ctx.drawImage(bg, 0, 0);
-        ctx.drawImage(fg, 0, cvs.height - fg.height);
-        ctx.drawImage(playButton, 90, cvs.height - fg.height - 50, 100, 50);
-        ctx.drawImage(
-            bird,
-            cvs.width / 2 - bird.width / 2,
-            cvs.height - fg.height - bird.height - 120
-        );
-        ctx.font = "40px impact"; // Candara, Roboto, Courier, Arial
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 4;
-        ctx.strokeText("FlappyBird", 55, 180);
-        ctx.fillStyle = "white";
-        ctx.fillText("FlappyBird", 55, 180);
+    if (gameBegin === true) {
+        function drawStart() {
+            ctx.drawImage(bg, 0, 0);
+            ctx.drawImage(fg, 0, cvs.height - fg.height);
+            ctx.drawImage(playButton, 90, cvs.height - fg.height - 50, 100, 50);
+            ctx.drawImage(
+                bird,
+                cvs.width / 2 - bird.width / 2,
+                cvs.height - fg.height - bird.height - 120
+            );
+            ctx.font = "40px impact"; // Candara, Roboto, Courier, Arial
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 4;
+            ctx.strokeText("FlappyBird", 55, 180);
+            ctx.fillStyle = "white";
+            ctx.fillText("FlappyBird", 55, 180);
 
-        requestAnimationFrame(drawStart);
+            requestAnimationFrame(drawStart);
+        }
+        drawStart();
     }
-    drawStart();
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
 
-    document.addEventListener("keydown", moveNext);
-    function moveNext() {
-        gameBegin = false;
-        gamePlay();
-    }
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "KeyP") {
+            gameBegin = false;
+            gameMiddle = true;
+            gamePlay();
+        }
+    });
+    document.addEventListener("mousedown", (ev) => {
+        if ((ev.clientX >= 90 && ev.clientX <= 190) && (ev.clientY >= cvs.height - fg.height - 50 && ev.clientY <= cvs.height - fg.height)) {
+            gameBegin = false;
+            gameMiddle = true;
+            gamePlay();
+        }
+    });
 }
 
 function playReset() {
-    // bX = 10;
-    // bY = 150;
-    // bPoints = 0;
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
     gameMiddle = false;
     gameEnding = true;
-    /* 
-    Reset the position of the pipes
-    */
+    gameEnd();
 }
 
 function gamePlay() {
-    function drawMiddle() {
-        // Drawing all the objects and interactions
-        ctx.drawImage(bg, 0, 0); // Draw background
+    if (gameMiddle === true) {
+        function drawMiddle() {
+            // Drawing all the objects and interactions
+            ctx.drawImage(bg, 0, 0); // Draw background
 
-        for (var i = 0; i < pipe.length; i++) {
-            constant = pipeNorth.height + gap;
-            ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
-            ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+            for (var i = 0; i < pipe.length; i++) {
+                constant = pipeNorth.height + gap;
+                ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+                ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
 
-            pipe[i].x--;
+                pipe[i].x--;
 
-            if (pipe[i].x === 100) {
-                pipe.push({
-                    x: cvs.width,
-                    y:
-                        Math.floor(Math.random() * pipeNorth.height) -
-                        pipeNorth.height,
-                });
+                if (pipe[i].x === 100) {
+                    pipe.push({
+                        x: cvs.width,
+                        y:
+                            Math.floor(Math.random() * pipeNorth.height) -
+                            pipeNorth.height,
+                    });
+                }
+
+                // Detect if there is a collision with pipes
+                if (
+                    (bX + bird.width >= pipe[i].x &&
+                        bX <= pipe[i].x + pipeNorth.width &&
+                        (bY <= pipe[i].y + pipeNorth.height ||
+                            bY + bird.height >= pipe[i].y + constant)) ||
+                    bY + bird.height >= cvs.height - fg.height
+                ) {
+                    ctx.clearRect(0, 0, cvs.width, cvs.height);
+                    gameMiddle = false;
+                    gameEnding = true;
+                    gameEnd();
+                }
+
+                if (pipe[i].x == 5) {
+                    bPoints++;
+                    score.play();
+                }
             }
+            ctx.drawImage(fg, 0, cvs.height - fg.height); // Foreground
+            ctx.drawImage(bird, bX, bY); // Draw bird
+            ctx.font = "40px impact"; //    -
+            ctx.strokeStyle = "black"; //    -
+            ctx.lineWidth = 8; //   Draw
+            ctx.strokeText(bPoints, 130, 40); //  points
+            ctx.fillStyle = "#fff"; //    -
+            ctx.fillText(bPoints, 130, 40); //    -
+            bY += gravity; // Gravity keeping the bird falling
 
-            // Detect if there is a collision with pipes
-            if (
-                (bX + bird.width >= pipe[i].x &&
-                    bX <= pipe[i].x + pipeNorth.width &&
-                    (bY <= pipe[i].y + pipeNorth.height ||
-                        bY + bird.height >= pipe[i].y + constant)) ||
-                bY + bird.height >= cvs.height - fg.height
-            ) {
-                playReset();
-            }
-            // Detect collision with ground
-            if (bX + bird.with >= bg.x) {
-                playReset();
-            }
-
-            if (pipe[i].x == 5) {
-                bPoints++;
-                score.play();
-            }
+            requestAnimationFrame(drawMiddle);
         }
-        ctx.drawImage(fg, 0, cvs.height - fg.height); // Foreground
-        ctx.drawImage(bird, bX, bY); // Draw bird
-        ctx.font = "40px impact"; //    -
-        ctx.strokeStyle = "black"; //    -
-        ctx.lineWidth = 8; //   Draw
-        ctx.strokeText(bPoints, 130, 40); //  points
-        ctx.fillStyle = "#fff"; //    -
-        ctx.fillText(bPoints, 130, 40); //    -
-        bY += gravity; // Gravity keeping the bird falling
-
-        requestAnimationFrame(drawMiddle);
+        drawMiddle();
     }
-    drawMiddle();
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
 
     // When key is down
-    document.addEventListener("keydown", moveUp);
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "ArrowUp" || event.code === "Space") {
+            bY -= 25;
+            fly.play();
+        }
+    });
 
-    function moveUp() {
-        bY -= 25;
-        fly.play();
-    }
-}
-
-function gameEnd() {
-    // End game screen, score and restart option
-    function drawEnd() {
-        ctx.drawImage(bg, 0, 0);
-        ctx.fillStyle = "#e8db84";
-        ctx.fillRect(48, 156, 192, 200);
-        // End text
-        requestAnimationFrame(drawEnd);
-    }
-    drawEnd();
-}
-
-function gameLoop(p1, p2, p3) {
-    if (p1 === true) {
-        gameStart();
-    } else if (p2 === true) {
-        gamePlay();
-    } else if (p3 === true) {
+    // Colliding with the ground
+    if (bX + bird.with >= bg.x) {
+        ctx.clearRect(0, 0, cvs.width, cvs.height);
+        gameMiddle = false;
+        gameEnding = true;
         gameEnd();
     }
 }
 
-gameLoop(gameBegin, gameMiddle, gameEnding);
-// gamePlay();
+function gameEnd() {
+    //ctx.clearRect(0, 0, cvs.width, cvs.height);
+    if (gameEnding === true) {
+        // End game screen, score and restart option
+        function drawEnd() {
+            ctx.drawImage(bg, 0, 0);
+            ctx.fillStyle = "#fff";  // #e8db84
+            ctx.fillRect(48, 156, 192, 200);
+            // End text
+            requestAnimationFrame(drawEnd);
+        }
+        drawEnd();
+    }
+}
+
+gameStart();
