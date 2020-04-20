a580d95f8e876d16437b0c35720544571be2ab50
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-20 21:40:04 +0100

    Update sprite.js

--- Javascript/Platformer/sprite.js
+++ Javascript/Platformer/sprite.js
@@ -1,27 +1,38 @@
 export default class Sprite {
-    constructor() {
-        this.dX = 0;
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.dX = 5;
         this.dY = 0;
         this.gravity = 5;
         this.health = 50;
 
         this.position = {
             x: 100,
-            y: 300
+            y: 300,
         };
     }
 
-    display(img, ctx) {
-        ctx.drawImage(img, this.position.x, this.position.y);
-        if (this.position.y + img.height >= 500) {
-            this.position.y = 500 - img.height;
-        } else {
-            this.position.y += this.gravity;
+    display(ctx) {
+        //ctx.drawImage(img, this.position.x, this.position.y);
+        this.position.y += this.gravity;
+        if (this.position.y >= 450) {
+            this.position.y = 450;
         }
+        ctx.fillStyle = "purple";
+        ctx.fillRect(this.position.x, this.position.y, 20, 50);
     }
 
-    jump() {
-        this.position.y -= 15;
+    moveLeft(ctx, deltaTime) {
+        this.position.x -= 1 / deltaTime;
+    }
+
+    moveRight(ctx, deltaTime) {
+        this.position.x += 1 / deltaTime;
+    }
+
+    jump(ctx, deltaTime) {
+        this.position.y -= 1 / deltaTime;
     }
 
     update(deltaTime) {
