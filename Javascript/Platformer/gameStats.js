e6fffe9205220d081f21650be18f4eaf60ab4063
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-21 00:42:05 +0100

    Update gameStats.js

--- Javascript/Platformer/gameStats.js
+++ Javascript/Platformer/gameStats.js
@@ -6,24 +6,21 @@ export default class gameStats {
         this.pause = new Image();
         this.healthBar.src = "images/healthbar.png";
         this.pause.src = "images/pause.png";
+        this.hp = 140;
     }
 
     display(ctx) {
         // Health bar
-        ctx.drawImage(this.healthBar, 594, 17);
-        ctx.drawImage(this.pause, 13, 13);
-
+        ctx.fillStyle = "#29de00";
+        ctx.fillRect(628, 25, this.hp, 19);
+        ctx.drawImage(this.healthBar, 600, 17);
+        if (this.hp <= 0) {
+            this.hp = 0;
+        }
         // Points
 
         // Game pause button
-        // ctx.strokeStyle = "#498699";
-        // ctx.lineWidth = 4;
-        // ctx.strokeRect(15, 15, 30, 30);
-        // ctx.fillStyle = "#8cc1d1";
-        // ctx.fillRect(17, 17, 26, 26);
-        // ctx.fillStyle = "#498699";
-        // ctx.fillRect(22, 21, 5, 18);
-        // ctx.fillRect(33, 21, 5, 18);
+        ctx.drawImage(this.pause, 17, 17);
 
         // Maybe inventory
 
