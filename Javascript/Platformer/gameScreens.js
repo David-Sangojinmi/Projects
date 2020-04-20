4c91d8cf079b2897f6a7913264c7fa9051bf288e
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-20 21:39:59 +0100

    Create gameScreens.js

---
+++ Javascript/Platformer/gameScreens.js
@@ -0,0 +1,30 @@
+export default class gameScreens {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+    }
+
+    startScreen(ctx) {
+        // Health bar
+        
+
+        // Points
+
+        
+        // Pause game button
+
+        // Maybe inventory
+    }
+
+    instructions(ctx) {
+
+    }
+
+    endScreen(ctx) {
+
+    }
+
+    update(deltaTime) {
+        if (!deltaTime) return;
+    }
+}
