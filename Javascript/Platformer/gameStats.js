35db47e96ffa438eea76814a25ec21d5a08d974d
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-20 15:16:31 +0100

    Initial commit

---
+++ Javascript/Platformer/gameStats.js
@@ -0,0 +1,23 @@
+export default class gameScreens {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+    }
+
+    /* display(ctx) {
+        // Health bar
+        
+
+        // Points
+
+        
+        // Pause game button
+
+
+        // Maybe inventory
+    } */
+
+    update(deltaTime) {
+        if (!deltaTime) return;
+    }
+}
