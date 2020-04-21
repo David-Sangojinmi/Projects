554e7edd12f2034508076249eae74629fe2fe388
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-21 21:43:36 +0100

    Update gameScreens.js

--- Javascript/Platformer/gameScreens.js
+++ Javascript/Platformer/gameScreens.js
@@ -2,21 +2,22 @@ export default class gameScreens {
     constructor(gameWidth, gameHeight) {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
+        this.gameTitle = new Image();
+        this.gameTitle.src = "images/gameTitle.png";
     }
 
     startScreen(ctx) {
-        // Health bar
-        
-
-        // Points
-
-        
-        // Pause game button
-
-        // Maybe inventory
+        // Title
+        ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+        ctx.fillRect(0, 0, 800, 600);
+        ctx.drawImage(this.gameTitle, 125, 225);
     }
 
     instructions(ctx) {
+
+    }
+
+    pauseScreen(ctx) {
 
     }
 
