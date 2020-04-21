596514ccc6a7bac68483cfa6f78e656f4bc01dcb
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-21 01:05:40 +0100

    Update gameStats.js

--- Javascript/Platformer/gameStats.js
+++ Javascript/Platformer/gameStats.js
@@ -1,12 +1,18 @@
 export default class gameStats {
     constructor(gameWidth, gameHeight) {
+        // Variables
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
+        this.hp = 140;
+        this.points = 0;
+
+        // Loading images
         this.healthBar = new Image();
         this.pause = new Image();
+        this.coin = new Image();
         this.healthBar.src = "images/healthbar.png";
         this.pause.src = "images/pause.png";
-        this.hp = 140;
+        this.coin.src = "images/coin.png";
     }
 
     display(ctx) {
@@ -17,7 +23,12 @@ export default class gameStats {
         if (this.hp <= 0) {
             this.hp = 0;
         }
+
         // Points
+        ctx.drawImage(this.coin, 600, 61);
+        ctx.font = "30px candara";
+        ctx.fillStyle = "white";
+        ctx.fillText("x " + this.points, 640, 85);
 
         // Game pause button
         ctx.drawImage(this.pause, 17, 17);
