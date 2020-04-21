e34d6a528ec574b7368928aab428408e1d295987
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-22 00:03:01 +0100

    Update platform.js

--- Javascript/Platformer/platform.js
+++ Javascript/Platformer/platform.js
@@ -2,9 +2,9 @@ export default class Platform {
     constructor(gameWidth, gameHeight, r, g, b) {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.r = 255;
-        this.g = 255;
-        this.b = 255;
+        this.r = this.r;
+        this.g = this.g;
+        this.b = this.b;
         this.width = 800;
         this.height = 100;
         this.scrollSpeed = 5;
@@ -20,32 +20,15 @@ export default class Platform {
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
         ctx.fillStyle = "rgb(84, 38, 193)";
         ctx.fillRect(this.position.x + 400, this.position.y - 40, 20, 40);
+        
     }
 
     scrollLeft(ctx) {
-        this.r++;
-        this.g--;
-        this.b++;
-        // ctx.clearRect(
-        //     this.position.x,
-        //     this.position.y,
-        //     this.width,
-        //     this.height
-        // );
-        //this.position.x += this.scrollSpeed;
+        this.position.x += this.scrollSpeed;
     }
 
     scrollRight(ctx) {
-        this.r--;
-        this.g++;
-        this.b--;
-        // ctx.clearRect(
-        //     this.position.x,
-        //     this.position.y,
-        //     this.width,
-        //     this.height
-        // );
-        //this.position.x -= this.scrollSpeed;
+        this.position.x -= this.scrollSpeed;
     }
 
     update(deltaTime) {
