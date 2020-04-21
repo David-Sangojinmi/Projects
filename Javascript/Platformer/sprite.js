3ffca1d9e73b079b37e8d366d27a7422540cdc83
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-21 21:44:08 +0100

    Update sprite.js

--- Javascript/Platformer/sprite.js
+++ Javascript/Platformer/sprite.js
@@ -2,10 +2,10 @@ export default class Sprite {
     constructor(gameWidth, gameHeight) {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.dX = 5;
-        this.dY = 0;
-        this.gravity = 5;
-        this.health = 50;
+        this.dX = 1;
+        this.dY = 70;
+        this.gravity = 3;
+        // this.health = 50;    >> Already defined in gameScreen
 
         this.position = {
             x: 100,
@@ -13,26 +13,26 @@ export default class Sprite {
         };
     }
 
+    moveLeft(ctx) {
+        this.position.x -= this.dX;
+    }
+
+    moveRight(ctx) {
+        this.position.x += this.dX;
+    }
+
+    jump(ctx) {
+        this.position.y -= this.dY;
+    }
+
     display(ctx) {
-        //ctx.drawImage(img, this.position.x, this.position.y);
+        ctx.fillStyle = "purple";
+        ctx.fillRect(this.position.x, this.position.y, 20, 50);
+        // ctx.drawImage(img, this.position.x, this.position.y);
         this.position.y += this.gravity;
         if (this.position.y >= 450) {
             this.position.y = 450;
         }
-        ctx.fillStyle = "purple";
-        ctx.fillRect(this.position.x, this.position.y, 20, 50);
-    }
-
-    moveLeft(ctx, deltaTime) {
-        this.position.x -= 1 / deltaTime;
-    }
-
-    moveRight(ctx, deltaTime) {
-        this.position.x += 1 / deltaTime;
-    }
-
-    jump(ctx, deltaTime) {
-        this.position.y -= 1 / deltaTime;
     }
 
     update(deltaTime) {
