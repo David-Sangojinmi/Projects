321e5326b54ea29fc33c6645682024bde39d122f
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-04-22 13:30:10 +0100

    Attempted to create array for terrain generation

--- Javascript/Platformer/index.js
+++ Javascript/Platformer/index.js
@@ -15,9 +15,6 @@ import Platform from "./platform.js";
 import Sprite from "./sprite.js";
 import gameScreens from "./gameScreens.js";
 import gameStats from "./gameStats.js";
-let sprite = new Sprite(GAME_WIDTH, GAME_HEIGHT);
-let gScreens = new gameScreens(GAME_WIDTH, GAME_HEIGHT);
-let gStats = new gameStats(GAME_WIDTH, GAME_HEIGHT);
 
 // Load all the images
 var background = new Image();
@@ -30,30 +27,25 @@ background.src = "images/bg6-3.jpg";
 //     jumpS.src = "sounds/jump.mp3";
 
 // Important variables
-var gamestart = true;
-var gameplay = false;
+let platform = new Platform(GAME_WIDTH, GAME_HEIGHT);
+let sprite = new Sprite(GAME_WIDTH, GAME_HEIGHT);
+let gScreens = new gameScreens(GAME_WIDTH, GAME_HEIGHT);
+let gStats = new gameStats(GAME_WIDTH, GAME_HEIGHT);
+var gamestart = false;
+var gameplay = true;
 var gamepaused = false;
 var gameend = false;
 var lastTime = 0;
-var platform = [];
-for (var i = 0; i < 6; i++) {
-    platform[i] = new Platform(GAME_WIDTH, GAME_HEIGHT, 8, 67, 168);
-    platform[i].position.x = 0 + 800 * i;
-    platform[i].position.y = 500;
-}
+// var platform = [];
+// for (var i = 0; i < 6; i++) {
+//     platform[i] = new Platform(GAME_WIDTH, GAME_HEIGHT, 8, 67, 168);
+//     platform[i].position.x = 0 + 800 * i;
+//     platform[i].position.y = 500;
+// }
 
 ///////////////////////////////
 ////  GAMEPLAY FUNCTIONS   ////
 ///////////////////////////////
-function coinCollision() {
-    if (sprite.position.x >= gStats.coinX && sprite.position.x <= gStats.coin + 50) {
-        gStats.points += 1;
-        //Coin.hide();
-    } else {
-        gStats.points += 0;
-    }
-}
-
 function gameStart(timestamp) {
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
@@ -80,23 +72,38 @@ document.addEventListener("click", (ev) => {
     }
 });
 
+function coinCollision() {
+    if (
+        sprite.position.x >= gStats.coinX &&
+        sprite.position.x <= gStats.coin + 50
+    ) {
+        gStats.points += 1;
+        //Coin.hide();
+    } else {
+        gStats.points += 0;
+    }
+}
+
 function gamePlay(timestamp) {
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
 
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-    ctx.drawImage(background, 0, 0);
+    // ctx.drawImage(background, 0, 0);
+    
+    platform.drawTP(ctx);
+
     gStats.update(deltaTime);
     gStats.display(ctx);
 
-    for (var i = 0; i < platform.length; i++) {
-        platform[i].update(deltaTime);
-        platform[i].draw(ctx);
+    // for (var i = 0; i < platform.length; i++) {
+    //     platform[i].update(deltaTime);
+    //     platform[i].draw(ctx);
 
-        if (platform[i].position.x === -800) {
-            platform[i].position.x = 800;
-        }
-    }
+    //     if (platform[i].position.x === -800) {
+    //         platform[i].position.x = 800;
+    //     }
+    // }
 
     sprite.update(deltaTime);
     sprite.display(ctx, deltaTime);
@@ -104,10 +111,6 @@ function gamePlay(timestamp) {
     coinCollision();
 
     window.requestAnimationFrame(gamePlay);
-}
-
-function gameEnd() {
-    //////
 }
 
 function gamePaused() {
@@ -149,6 +152,10 @@ document.addEventListener("click", (evnt) => {
     }
 });
 
+function gameEnd() {
+    //////
+}
+
 ///////////////////////////////
 ////     GAMEPLAY INIT     ////
 ///////////////////////////////
@@ -164,4 +171,5 @@ function gameLoop() {
     }
 }
 
-gameLoop();
+// gameLoop();
+gamePlay();
--- Javascript/Platformer/platform.js
+++ Javascript/Platformer/platform.js
@@ -8,6 +8,17 @@ export default class Platform {
         this.width = 800;
         this.height = 100;
         this.scrollSpeed = 5;
+        this.tpBlock = {
+            width: 50,
+            height: 120
+        };
+        this.testPlatform = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2]
+        ];
 
         this.position = {
             x: 0,
@@ -21,7 +32,60 @@ export default class Platform {
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
         ctx.fillStyle = "rgb(84, 38, 193)";
         ctx.fillRect(this.position.x + 400, this.position.y - 40, 20, 40);
-        
+    }
+
+    drawTP(ctx) {
+        for (var i = 0; i < this.testPlatform.length; i++) {
+            var innertestPlatform = this.testPlatform[i];
+            for (var j = 0; innertestPlatform.length; j++) {
+                if (this.testPlatform[i][j] == 2) {
+                    ctx.fillStyle = "#5c2b00";
+                    ctx.fillRect(
+                        0, //  + this.tpBlock.width * j
+                        0, //  + this.tpBlock.height * i
+                        50,
+                        120
+                    );
+                }
+                if (this.testPlatform[i][j] == 1) {
+                    ctx.fillStyle = "#005c06";
+                    ctx.fillRect(100, 100, 50, 120);
+                }
+                if (this.testPlatform[i][j] == 0) {
+                    ctx.fillStyle = "#ffffff";
+                    ctx.fillRect(100, 200, 50, 120);
+                }
+                // switch (this.testPlatform[i][j]) {
+                //     case 2: // Terrain
+                //         ctx.fillStyle = "#5c2b00";
+                //         ctx.fillRect(
+                //             0 + this.tpBlock.width * j,
+                //             0 + this.tpBlock.height * i,
+                //             this.tpBlock.width,
+                //             this.tpBlock.height
+                //         );
+                //         break;
+                //     case 1: // Grass
+                //         ctx.fillStyle = "#005c06";
+                //         ctx.fillRect(
+                //             0 + this.tpBlock.width * j,
+                //             0 + this.tpBlock.height * i,
+                //             this.tpBlock.width,
+                //             this.tpBlock.height
+                //         );
+                //         break;
+                //     case 0: // Air/Invisible
+                //         ctx.fillStyle = "rgba(0, 0, 0, 0)";
+                //         ctx.fillRect(
+                //             0 + this.tpBlock.width * j,
+                //             0 + this.tpBlock.height * i,
+                //             this.tpBlock.width,
+                //             this.tpBlock.height
+                //         );
+                //         break;
+                // }
+            }
+        }
     }
 
     scrollLeft(ctx) {
